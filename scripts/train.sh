3068f82176edb91b37b534d3c204279c1f69db72
Muyang Yan <muyangyan1@gmail.com>
2024-07-25 00:46:40 -0400

    scripts

---
+++ scripts/train.sh
@@ -0,0 +1,1 @@
+python main_nusc.py --task long --seg --anticipate --pos_emb --n_query 20 --n_encoder_layer 2 --n_decoder_layer 2 --batch_size 8 --hidden_dim 512 --max_pos_len 3100 --sample_rate 6 --epochs 70 --mode=train --input_type=nusc_bitmasks_scenegraphs --split=1
